c357c1f7ce7f5550fb8214fc59d1a227a2f326ce
Modifying <baiwfnhzxl@gmail.com>
2018-04-18 09:26:37 +0800

    print function

---
+++ kernel/main.c
@@ -0,0 +1,21 @@
+#include "../lib/kernel/print.h"
+
+
+
+int main() 
+{
+    put_str("I am kernel\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
+    while(1);
+
+    return 0;
+}
+
---
+++ lib/kernel/print.h
@@ -0,0 +1,10 @@
+#ifndef _LIB_KERNEL_PRINT_H_
+#define _LIB_KERNEL_PRINT_H_
+
+#include "../stdint.h"
+
+void put_char(char ch_asci);
+void put_str(char *str);
+void put_int(unsigned int num);
+
+#endif //!_LIB_KERNEL_PRINT_H_
